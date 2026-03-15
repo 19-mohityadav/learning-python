d414dc4aabbdd91b24a877823e99125006b067cc
Mohit Yadav <yadavmohit85692@gmail.com>
2026-03-15 18:39:20 +0530

    Dictionary in Py

---
+++ 24_Dictionary.py
@@ -0,0 +1,42 @@
+# Dictionary = a collection of {key: value} pairs
+#              ordered and changeable. No Duplicates
+
+capitals = {
+    "India": "New Delhi",
+    "China": "Beijing",
+    "Russia": "Moscow",
+    "Japan": "Tokyo",
+}
+
+# print(dir(capitals))
+# print(help(capitals))
+
+# print(capitals.get("India"))
+# print(capitals.get("China"))
+# print(capitals.get("USA"))
+
+# if capitals.get("USA"):
+#     print("That capital exists")
+# else:
+#     print("That capital doesn't exists")
+
+# capitals.update({"Germany": "Berlin"})
+# capitals.update({"France": "Paris"})
+# print(capitals)
+
+# capitals.pop("China")
+# print(capitals)
+# capitals.popitem()
+# print(capitals)
+
+# keys = capitals.keys() # print(keys)
+# for key in keys:
+#     print(key)
+
+# values = capitals.values() # print(values)
+# for value in values:
+#     print(value)
+
+# items = capitals.items() # print(items)
+# for key, value in items:
+#     print(f"{key}: {value}")

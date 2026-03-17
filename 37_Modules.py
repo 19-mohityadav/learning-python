7fe1f29d8f0027f7ea6c725911c28bee6f631200
Mohit Yadav <yadavmohit85692@gmail.com>
2026-03-17 19:46:26 +0530

    module

---
+++ 37_Modules.py
@@ -0,0 +1,15 @@
+# module --> A file containing code you want to include in your program
+#            use 'import' to include a module (built-in or your own)
+#            useful to break up a large program into reusable separate files
+
+# print(help("modules"))
+
+# import math
+# print(math.ceil(math.pi))
+
+
+# import math as m
+# print(m.sqrt(5))
+
+# from math import pi #Not recomended
+# print(pi)

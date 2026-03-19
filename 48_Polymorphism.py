4d9251869c1b521ada17f4fb11750cc8b3741204
Mohit Yadav <yadavmohit85692@gmail.com>
2026-03-19 18:54:55 +0530

    Polymorphism in py

---
+++ 48_Polymorphism.py
@@ -0,0 +1,39 @@
+# Polymorphism --> That means many forms or Faces
+#                   Poly -> Many & Morph -> Form
+#                   Two ways to activate Polymorphism
+#                   1. Inheritance --> An object could be treated of the same type as a parent class
+#                   2. "Duck typing" --> Object must have necessary attributes/methods
+
+from abc import ABC, abstractmethod
+
+class Shape:
+
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * (self.radius ** 2)
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+class Triangle(Shape):
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return self.base * self.height * 0.5
+
+shapes = [Circle(4), Square(5), Triangle(3, 4)]
+
+print(shapes[0].area())
